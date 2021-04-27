902875ea0792134b9fe1cdd45fe869612d3277f5
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-27 19:12:33 +0300

    Create a Promo component for Mainpage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import Header from "../Header/Header"
+import Header from "../Header/Header";
+import Promo from "../Promo/Promo";
 
 function App() {
     return (
     <>
         <Header/>
+        <Promo/>
     </>
     )
 }
---
+++ src/components/Promo/Promo.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Promo.css";
+
+function Promo() {
+    return (
+        <section className="promo">
+            <h1 className="promo__title">
+                Учебный проект студента факультета Веб-разработки.
+            </h1>
+            <div className="promo__banner" />
+        </section>
+    );
+}
+
+export default Promo;
