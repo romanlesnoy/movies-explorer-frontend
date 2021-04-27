dbaf20e009b2ed843b97024b6bdd86a68c676948
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-27 16:55:03 +0300

    Fix paths to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../images/icons/logo.svg";
+import logo from "../../images/icons/logo.svg";
 import "./Header.css";
 
 function Header() {
