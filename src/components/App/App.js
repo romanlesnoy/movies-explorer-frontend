4e2c3307ab78dfb52c8f84fbbb27264090e19165
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-28 16:04:43 +0300

    Create a AboutProject component for the Main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
 import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
 
 function App() {
     return (
     <>
         <Header/>
         <Promo/>
+        <AboutProject/>
     </>
     )
 }
