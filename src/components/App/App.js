686374dde21fc0eb37f0ac18f334bb5b744be088
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-30 18:32:32 +0300

    Create a Footer component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import Header from "../Header/Header";
-import Promo from "../Promo/Promo";
-import AboutProject from "../AboutProject/AboutProject";
-import Techs from "../Techs/Techs";
-import AboutMe from "../AboutMe/AboutMe";
+// import Promo from "../Promo/Promo";
+// import AboutProject from "../AboutProject/AboutProject";
+// import Techs from "../Techs/Techs";
+// import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
+import Footer from "../Footer/Footer";
 
 function App() {
     return (
     <>
         <Header/>
-        <Promo/>
+        {/* <Promo/>
         <AboutProject/>
         <Techs/>
-        <AboutMe/>
+        <AboutMe/> */}
         <Portfolio/>
+        <Footer/>
     </>
     )
 }
