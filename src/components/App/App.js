3350838279bc6b3fd05d523796304ecdfd122cea
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-30 18:45:20 +0300

    Fix width tech__list

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Header from "../Header/Header";
-// import Promo from "../Promo/Promo";
-// import AboutProject from "../AboutProject/AboutProject";
-// import Techs from "../Techs/Techs";
-// import AboutMe from "../AboutMe/AboutMe";
+import Promo from "../Promo/Promo";
+import AboutProject from "../AboutProject/AboutProject";
+import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 import Portfolio from "../Portfolio/Portfolio";
 import Footer from "../Footer/Footer";
 
@@ -11,10 +11,10 @@ function App() {
     return (
     <>
         <Header/>
-        {/* <Promo/>
+        <Promo/>
         <AboutProject/>
         <Techs/>
-        <AboutMe/> */}
+        <AboutMe/>
         <Portfolio/>
         <Footer/>
     </>
