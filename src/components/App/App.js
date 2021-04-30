de56ba5d7e2252453e19b0edcd978c43fc33e5d7
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-30 13:43:01 +0300

    Create a AboutMe component for the Main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import Header from "../Header/Header";
-import Promo from "../Promo/Promo";
-import AboutProject from "../AboutProject/AboutProject";
-import Techs from "../Techs/Techs";
+// import Promo from "../Promo/Promo";
+// import AboutProject from "../AboutProject/AboutProject";
+// import Techs from "../Techs/Techs";
+import AboutMe from "../AboutMe/AboutMe";
 
 function App() {
     return (
     <>
         <Header/>
-        <Promo/>
+        {/* <Promo/>
         <AboutProject/>
-        <Techs/>
+        <Techs/> */}
+        <AboutMe/>
     </>
     )
 }
