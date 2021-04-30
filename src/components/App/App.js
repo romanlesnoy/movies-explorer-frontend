ee5e6f2a6b5b5d6e8b71902b75f20b63095ddda2
Roman Lesnoy <romanlesnoy@outlook.com>
2021-04-30 19:19:29 +0300

    Add Main component, create Route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import Header from "../Header/Header";
-import Promo from "../Promo/Promo";
-import AboutProject from "../AboutProject/AboutProject";
-import Techs from "../Techs/Techs";
-import AboutMe from "../AboutMe/AboutMe";
-import Portfolio from "../Portfolio/Portfolio";
+import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 
 function App() {
+    const [loggedIn, setLoggedIn] = useState(true);
     return (
-    <>
-        <Header/>
-        <Promo/>
-        <AboutProject/>
-        <Techs/>
-        <AboutMe/>
-        <Portfolio/>
-        <Footer/>
-    </>
-    )
+        <>
+            <Header />
+
+            <Switch>
+                <Route exact path="/">
+                    <Main />
+                </Route>
+
+                <Route path="/sign-up">
+                    <h1>Регистрация</h1>
+                </Route>
+
+                <Route path="/sign-in">
+                    <h1>Логин</h1>
+                </Route>
+
+                <Route path="/movies">
+                    <h1>movies</h1>
+                </Route>
+                
+                <Route path="*">
+                    <h1> Страница 404</h1>
+                </Route>
+            </Switch>
+
+            <Footer />
+        </>
+    );
 }
 
 export default App;
