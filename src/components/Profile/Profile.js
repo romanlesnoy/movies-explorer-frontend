06dc4098c1606674061bed4884d4d7b7204f968b
Roman Lesnoy <romanlesnoy@outlook.com>
2021-05-04 18:18:58 +0300

    Create a Profile component

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Profile.css";
+import { Link } from "react-router-dom";
+import Header from "../Header/Header";
+
+function Profile() {
+    return (
+        <section className="profile">
+            <h2 className="profile__welcome">Привет, Виталий!</h2>
+            <form className="profile__edit-form">
+                <label className="profile__label">
+                    Имя
+                    <input
+                        className="profile__input"
+                        name="name"
+                        type="text"
+                        placeholder="Имя"
+                        required
+                    />
+                </label>
+                <hr className="profile__divider" />
+                <label className="profile__label">
+                    Почта
+                    <input
+                        className="profile__input"
+                        name="email"
+                        type="email"
+                        placeholder="Email"
+                        required
+                    />
+                </label>
+            </form>
+            <div className="profile__buttons-container">
+                <button type="submit" className="profile__button">
+                    Редактировать
+                </button>
+                <button type="button" className="profile__button" role="link">
+                    <Link className="profile__button-link"to="/login">Выйти из аккаунта</Link>
+                </button>
+            </div>
+        </section>
+    );
+}
+
+export default Profile;
