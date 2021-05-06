6d1e6959671b5e0c59e5248f1cf18e4de039da0e
Roman Lesnoy <romanlesnoy@outlook.com>
2021-05-06 16:13:05 +0300

    Create a Navigation component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,37 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import logo from "../../images/icons/logo.svg";
 import "./Header.css";
+import Navigation from "../Navigation/Navigation";
 
-function Header({loggedIn}) {
+function Header({ loggedIn }) {
     const headerBackgroundColoreStyle = loggedIn
         ? "header header_logged-in"
         : "header";
+
+    const [isNavPopupIsOpen, setIsNavPopupOpen] = useState(false);
+
+    const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
+    function handleBurgerButtonClick() {
+        setIsNavPopupOpen(true);
+    }
+
+    function closeNavPopup() {
+        setIsNavPopupOpen(false);
+    }
     return (
         <header className={headerBackgroundColoreStyle}>
             <Link to="/" className="header__link">
                 <img className="header__logo" src={logo} alt="Логотип проект" />
             </Link>
             {loggedIn ? (
-                <ul className="header_links">
-                    <li className="header__link-item">
-                        <Link to="/movies" className="header__link">
-                            Фильмы
-                        </Link>
-                    </li>
-                    <li className="header__link-item">
-                        <Link to="/saved-movies" className="header__link">
-                            Сохранённые фильмы
-                        </Link>
-                    </li>
-                    <li className="header__link-item header__link-item">
-                        <Link to="/profile" className="header__link header__profile-link">
-                            Аккаунт
-                            <button className="header__profile-button"/>
-                        </Link>
-                    </li>
-                </ul>
-            ) : (
+                <>
+                    {!isMobile && (//разметка loggedin && desktop resolution
+                        <ul className="header_links">
+                            <li className="header__link-item">
+                                <Link to="/movies" className="header__link">
+                                    Фильмы
+                                </Link>
+                            </li>
+                            <li className="header__link-item">
+                                <Link
+                                    to="/saved-movies"
+                                    className="header__link"
+                                >
+                                    Сохранённые фильмы
+                                </Link>
+                            </li>
+                            <li className="header__link-item header__link-item">
+                                <Link
+                                    to="/profile"
+                                    className="header__link header__profile-link"
+                                >
+                                    Аккаунт
+                                    <button className="header__profile-button" />
+                                </Link>
+                            </li>
+                        </ul>
+                    )}
+                    {isMobile && ( //разметка loggedin && mobile resolution
+                        <>
+                            <button
+                                className="header__burger-button"
+                                aria-label="Open navigation menu"
+                                type="button"
+                                onClick={handleBurgerButtonClick}
+                            ></button>
+                            <Navigation
+                                isOpen={isNavPopupIsOpen}
+                                onClose={closeNavPopup}
+                            />
+                        </>
+                    )}
+                </>
+            ) : ( //разметка loggedOut
                 <ul className="header_links">
                     <li className="header__link-item">
                         <Link to="sign-up" className="header__link">
