1b8e19fa3279a85cba421da188e382d4de42e5bb
Roman Lesnoy <romanlesnoy@outlook.com>
2021-05-12 12:28:04 +0300

    Add form validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,8 +2,20 @@ import React from "react";
 import "./Register.css";
 import { Link } from "react-router-dom";
 import logo from "../../images/icons/logo.svg";
+import useFormValidation from "../../hooks/useFormValidation";
 
 function Register() {
+    const { values, errors, isValid, handleChange, resetForm } =
+        useFormValidation({});
+
+    function handleOnSubmit(evt) {
+        evt.preventDefault();
+        console.log(values);
+        resetForm();
+    }
+
+    console.log(errors.email);
+
     return (
         <section className="register">
             <Link to="/" className="register__home">
@@ -14,45 +26,55 @@ function Register() {
                 />
             </Link>
             <h2 className="register__welcome">Добро пожаловать!</h2>
-            <form className="register__form">
+            <form className="register__form" onSubmit={handleOnSubmit}>
                 <label className="register__label">
                     Имя
                     <input
-                        className="register__input"
+                        className={`register__input ${errors.name && "register__input_invalid"}`}
                         name="name"
                         type="text"
                         placeholder="Имя"
                         required
                         autoComplete="off"
+                        onChange={handleChange}
+                        value={values.name || ''}
                     />
-                    <span className="register__input-error"></span>
+                    <span className="register__input-error">{errors.name}</span>
                 </label>
                 <label className="register__label">
                     E-mail
                     <input
-                        className="register__input"
+                        className={`register__input ${errors.email && "register__input_invalid"}`}
                         name="email"
                         type="email"
                         placeholder="Email"
                         required
                         autoComplete="off"
+                        onChange={handleChange}
+                        value={values.email || ''}
                     />
-                    <span className="register__input-error"></span>
+                    <span className="register__input-error">{errors.email}</span>
                 </label>
                 <label className="register__label">
                     Пароль
                     <input
-                        className="register__input"
+                        className={`register__input ${errors.password && "register__input_invalid"}`}
                         name="password"
                         type="password"
                         minLength="8"
                         placeholder="Пароль"
                         required
                         autoComplete="off"
+                        onChange={handleChange}
+                        value={values.password || ''}
                     />
-                    <span className="register__input-error"></span>
+                    <span className="register__input-error">{errors.password}</span>
                 </label>
-                <button type="submit" className="register__button">
+                <button
+                    type="submit"
+                    className={`register__submit-button ${!isValid && "register__submit-button_disable"}`}
+                    disabled={!isValid}
+                >
                     Зарегистрироваться
                 </button>
                 <p className="register__paragraph">
