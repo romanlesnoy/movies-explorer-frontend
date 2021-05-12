cc3a2497353992d56e275e3ceeffa69033ab6599
Roman Lesnoy <romanlesnoy@outlook.com>
2021-05-12 13:48:14 +0300

    Add CurrentUserContext

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,  { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
@@ -7,39 +7,44 @@ import Profile from "../Profile/Profile";
 import Login from "../Login/Login";
 import Register from "../Register/Register";
 import NotFound from "../NotFound/NotFound";
+import { CurrentUserContext } from "../../context/currentUserContext";
 
 function App() {
+    const [currentUser, setCurrentUser] = useState({});
+
     return (
         <>
-            <Switch>
-                <Route exact path="/">
-                    <Main loggedIn={false}/>
-                </Route>
+            <CurrentUserContext.Provider value={currentUser}>
+                <Switch>
+                    <Route exact path="/">
+                        <Main loggedIn={false} />
+                    </Route>
 
-                <Route path="/sign-up">
-                    <Register />
-                </Route>
+                    <Route path="/sign-up">
+                        <Register />
+                    </Route>
 
-                <Route path="/sign-in">
-                    <Login />
-                </Route>
+                    <Route path="/sign-in">
+                        <Login />
+                    </Route>
 
-                <Route path="/profile">
-                    <Profile loggedIn={true}/>
-                </Route>
+                    <Route path="/profile">
+                        <Profile loggedIn={true} />
+                    </Route>
 
-                <Route path="/movies">
-                    <Movies loggedIn={true}/>
-                </Route>
+                    <Route path="/movies">
+                        <Movies loggedIn={true} />
+                    </Route>
 
-                <Route path="/saved-movies">
-                    <SavedMovies loggedIn={true}/>
-                </Route>
+                    <Route path="/saved-movies">
+                        <SavedMovies loggedIn={true} />
+                    </Route>
 
-                <Route path="*">
-                    <NotFound />
-                </Route>
-            </Switch>
+                    <Route path="*">
+                        <NotFound />
+                    </Route>
+                </Switch>
+            </CurrentUserContext.Provider>
         </>
     );
 }
