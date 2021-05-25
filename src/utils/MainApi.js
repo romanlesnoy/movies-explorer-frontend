0e9e5d6daf5319873e9038ef09c58f861bb4144c
Roman Lesnoy <romanlesnoy@outlook.com>
2021-05-25 18:48:52 +0300

    add personal info in profile component

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://api.movies-explorer.nomoredomains.icu";
-const BASE_URL = "http://localhost:3001";
+const BASE_URL = "https://api.movies-explorer.nomoredomains.icu";
+// const BASE_URL = "http://localhost:3001";
 
 const response = (res) => {
     if (res.ok) {
